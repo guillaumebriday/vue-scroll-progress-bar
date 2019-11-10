b7e72e94fb730757fcfe7f611945842bdc639825
Guillaume Briday <guillaumebriday@gmail.com>
2019-11-10 23:59:08 +0100

    Updating example

--- examples/src/main.js
+++ examples/src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueScrollProgressBar from "../../src"
+import VueScrollProgressBar from "../../dist/vue-scroll-progress-bar.es"
 
 Vue.config.productionTip = false
 
-// Vue.use(VueScrollProgressBar)
-Vue.component("vue-scroll-progress-bar", VueScrollProgressBar)
+Vue.use(VueScrollProgressBar)
+// Vue.component("vue-scroll-progress-bar", VueScrollProgressBar)
 
 new Vue({
   render: h => h(App),
